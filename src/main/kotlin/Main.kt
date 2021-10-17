1f58aa720a5aaeda89d0ee47dfeddc4eafbaf18e
demenyzsofia <79308025+demenyzsofia@users.noreply.github.com>
2021-10-17 21:03:20 +0300

    the ready tasks

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+
+
+fun main(args: Array<String>) {
+    val quizController= QuizController()
+    quizController.doQuiz(2)
+}
---
+++ src/main/kotlin/Question.kt
@@ -0,0 +1,2 @@
+
+data class Question( val text:String,var answer : List<String>)
